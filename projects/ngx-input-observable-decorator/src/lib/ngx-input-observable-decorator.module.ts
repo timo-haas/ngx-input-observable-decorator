96a5dc0fdc9826e5d2b090809e2b12081be7ba4f
Timo <simpel@outlook.com>
2019-04-27 19:14:21 +0200

    first implementation

--- projects/ngx-input-observable-decorator/src/lib/ngx-input-observable-decorator.module.ts
+++ projects/ngx-input-observable-decorator/src/lib/ngx-input-observable-decorator.module.ts
@@ -1,9 +1,59 @@
-import { NgModule } from '@angular/core';
-import { NgxInputObservableDecoratorComponent } from './ngx-input-observable-decorator.component';
+import { BehaviorSubject } from 'rxjs';
 
-@NgModule({
-  declarations: [NgxInputObservableDecoratorComponent],
-  imports: [],
-  exports: [NgxInputObservableDecoratorComponent]
-})
-export class NgxInputObservableDecoratorModule {}
+const subjectInstanceMap = new WeakMap<any, BehaviorSubject<any>>();
+const getInstanceSubject = (instance: any): BehaviorSubject<any> => {
+  const existingSubject = subjectInstanceMap.get(instance);
+  if (existingSubject) {
+    return existingSubject;
+  }
+  const instanceSubject = new BehaviorSubject<any>(undefined);
+  subjectInstanceMap.set(instance, instanceSubject);
+  return instanceSubject;
+};
+
+export function InputObservable<T>(options?: {
+  bindingObservablePropertyName?: string;
+}): any {
+  return function(
+    targetPrototype: T,
+    propertyName: keyof T,
+    descriptor: PropertyDescriptor
+  ) {
+    const observablePropertyName =
+      (options && options.bindingObservablePropertyName) || propertyName + '$';
+    const getterFx = function(this: T) {
+      return getInstanceSubject(this).getValue();
+    };
+    const setterFx = function(this: T, value: any): void {
+      return getInstanceSubject(this).next(value);
+    };
+
+    Object.defineProperty(targetPrototype, observablePropertyName, {
+      get: function(this: T) {
+        return getInstanceSubject(this).asObservable();
+      },
+      enumerable: true
+    });
+
+    if (descriptor) {
+      const originalSetter = descriptor.set;
+      const originalGetter = descriptor.get;
+      descriptor.get = getterFx;
+      descriptor.set = function(this: T, value: any) {
+        if (originalSetter) {
+          originalSetter.call(this, value);
+        }
+        if (originalGetter) {
+          value = originalGetter.call(this);
+        }
+        return setterFx.call(this, value);
+      };
+    } else {
+      Object.defineProperty(targetPrototype, propertyName, {
+        get: getterFx,
+        set: setterFx,
+        enumerable: true
+      });
+    }
+  };
+}
--- projects/ngx-input-observable-decorator/src/public-api.ts
+++ projects/ngx-input-observable-decorator/src/public-api.ts
@@ -1,6 +1,1 @@
-/*
- * Public API Surface of ngx-input-observable-decorator
- */
-
-export * from './lib/ngx-input-observable-decorator.component';
-export * from './lib/ngx-input-observable-decorator.module';
+export { InputObservable } from './lib/ngx-input-observable-decorator.module';
